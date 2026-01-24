64d4956ec70934d07b6d45e317858aec4596cd56
kplngyi <y.ding2@siat.ac.cn>
2026-01-25 03:08:42 +0800

    添加测试

---
+++ gemini.js
@@ -0,0 +1,52 @@
+async function request(method, params) {
+  return new Promise((resolve, reject) => {
+    const httpMethod = $httpClient[method.toLowerCase()];
+    httpMethod(params, (error, response, data) => {
+      resolve({ error, response, data });
+    });
+  });
+}
+
+async function main() {
+  const { error, response, data } = await request(
+    "GET",
+    "https://gemini.google.com"
+  );
+
+  if (error) {
+    $done({
+      content: "Network Error",
+      backgroundColor: "",
+    });
+    return;
+  }
+
+  if (response.status === 200) {
+    $done({
+      content: "Available",
+      backgroundColor: "#4285F4",
+    });
+    return;
+  }
+
+  if (response.status === 403 || response.status === 451) {
+    $done({
+      content: "Not Available",
+      backgroundColor: "",
+    });
+    return;
+  }
+
+  $done({
+    content: "Unknown Error",
+    backgroundColor: "",
+  });
+}
+
+(async () => {
+  main()
+    .then((_) => {})
+    .catch((error) => {
+      $done({});
+    });
+})();
